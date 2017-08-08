5da19ebea39ed4fba70196048f64033371220acf
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-08-08 14:45:46 -0300

    Add validations in gallows and list comprehension

--- gallows.py
+++ gallows.py
@@ -3,24 +3,37 @@ def game():
     print("Welcome to the gallows game")
     print("****************************")
 
-    secret_word = "banana"
-    letters_hit = ["_","_","_","_","_","_"]
+    secret_word = "banana".upper()
+    letters_hit = [ "_" for letter in secret_word ]
 
     hanged = False
     hit = False
+    errors = 0
+
+    print(letters_hit)
 
     while not hanged and not hit:
         kick = input("What is the letter? ")
-        kick = kick.strip()
-        index = 0
+        kick = kick.strip().upper()
 
-        for letter in secret_word:
-            if letter.upper() == kick.upper():
-                letters_hit[index] = letter
-            index = index + 1
+        if kick in secret_word:
+            index = 0
+            for letter in secret_word:
+                if letter == kick:
+                    letters_hit[index] = letter
+                index += 1
+        else:
+            errors += 1
 
+        hanged = errors == 6
+        hit = "_" not in secret_word
         print(letters_hit)
 
+
+    if hit:
+        print("You won")
+    else:
+        print("You lost")
 
     print("End of the game")
 
